462efd56cb7a53d2e13d55d2a85ff6dc025f0f35
Foo Wai Chong <76924240+Steven-C1998@users.noreply.github.com>
2021-12-27 20:08:41 -0500

    Add files via upload

---
+++ contracts/TestDemo.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestDemo is ERC20 {
+    address public admin;
+    constructor(uint256 _supply) ERC20 ("TestDemo", "TSTD"){
+        _mint(msg.sender, _supply * (10 ** decimals()));
+        admin = msg.sender;
+    }
+    
+    function mint(address to, uint amount) external {
+        require(msg.sender == admin, 'only admin');
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+
+}
